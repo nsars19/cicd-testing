95aad07aa4470f92efff4fb6c991a1cf40e83c08
Nick Sardonini <nsardonini@umassp.edu>
2021-09-30 14:10:21 -0400

    fix failing test

--- index.test.js
+++ index.test.js
@@ -4,6 +4,6 @@ test("Says hello", () => {
   expect(printHello()).toBe("Hello");
 });
 
-test("Fails", () => {
-  expect(printHello()).toBe("Goodbye");
+test("Doesn't say goodbye", () => {
+  expect(printHello()).not.toBe("Goodbye");
 });
